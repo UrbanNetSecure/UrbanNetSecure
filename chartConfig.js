63afb42f15e6f36d28e29802c6d2cb3f88fa804e
hunyoungcha <chamgf5247@naver.com>
2024-09-09 02:32:05 +0900

    #7 새로운 차트 추가, 새로운 차트 디자인 수정 필요

--- chartConfig.js
+++ chartConfig.js
@@ -1,25 +1,35 @@
-var chartUpdateInterval;
-const defaultLineData = [0, 0, 0, 0, 0, 0, 0];
-const defaultDoughnutData = [0, 0, 0];
+var chartUpdateInterval = null;
+var line_chart = null;
+var doughnut_chart = null;
+var pie_chart = null;
 
-const deviceData = {
+var defaultLineData = [0, 1, 0, 5, 0, 3, 2];
+var defaultMalLineData = [1, 5, 2, 6, 5, 8, 3];
+var defaultDoughnutData = [1, 2, 3];
+var testLineData = [10,20,30,40,50,60,70];
+
+
+var deviceData = {
     bexco : {
         lineData : defaultLineData,
-        doughnutData : defaultDoughnutData
+        doughnutData : defaultDoughnutData,
+        malLineData : defaultMalLineData
     },
     test1 : {
-        lineData : defaultLineData,
-        doughnutData : defaultDoughnutData
+        lineData : testLineData,
+        doughnutData : defaultDoughnutData,
+        malLineData : defaultMalLineData
     },
     test2 : {
         lineData : defaultLineData,
-        doughnutData : defaultDoughnutData
+        doughnutData : defaultDoughnutData,
+        malLineData : defaultMalLineData
     }
 }
 
-function showdoughnutChart(){
+function showdoughnutChart(markerKey){
     var ctx = document.getElementById('doughnut_chart').getContext('2d');
-    
+
     var data = {
         labels: [
           'Red',
@@ -28,7 +38,7 @@ function showdoughnutChart(){
         ],
         datasets: [{
             label: 'data',
-            data: defaultDoughnutData,
+            data: deviceData[markerKey].doughnutData,
             backgroundColor: [
                 'rgb(255, 99, 132)',
                 'rgb(54, 162, 235)',
@@ -49,17 +59,51 @@ function showdoughnutChart(){
     });
 }
 
-function showLineChart() {
+function showPieChart(markerKey) {
+    var ctx = document.getElementById('pie_chart').getContext('2d');
+
+    var data = {
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets: [{
+            label: 'Pie Chart Data',
+            data: deviceData[markerKey].doughnutData, // 도넛 차트와 동일한 데이터를 사용
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+        }]
+    };
+
+    pie_chart = new Chart(ctx, {
+        type: 'pie',
+        data: data,
+        options: {
+            responsive: false,
+            maintainAspectRatio: false
+        }
+    });
+}
+
+function showLineChart(markerKey) {
     var ctx = document.getElementById('line_chart').getContext('2d');
 
     var data = {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Data',
-            data: defaultLineData,
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1
-        }]
+        datasets: [
+            {
+                label: 'Data',
+                data: deviceData[markerKey].lineData,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            },
+            {
+                label: 'Data2',
+                data: deviceData[markerKey].malLineData,
+                borderWidth: 1
+
+            }]
     }
 
     line_chart = new Chart(ctx, {
@@ -69,26 +113,33 @@ function showLineChart() {
             animation: {
                 duration: 1000, 
                 easing: 'easeInOutQuad' 
-            }
+            },
         }
     });
 }
 
 // 1초마다 데이터 업데이트
-function startChartUpdate() {
+function startChartUpdate(markerKey) {
     chartUpdateInterval = setInterval(function () {
-        updateChartData();
+        updateChartData(markerKey);
     }, 1000); 
 }
 
-function updateChartData() {
-    var newData = generateRandomData(); 
-    line_chart.data.datasets[0].data = newData; 
-    line_chart.update(); 
+function updateChartData(markerKey) {
+    var newLineData = generateRandomData(); 
+    var newMalLineData = generateRandomData(); 
+    deviceData[markerKey].lineData = newLineData; 
+    deviceData[markerKey].malLineData = newMalLineData; 
 
-    var doughnutNewData = genRandData();
-    doughnut_chart.data.datasets[0].data=doughnutNewData;
-    doughnut_chart.update();
+    line_chart.data.datasets[0].data = deviceData[markerKey].lineData; 
+    line_chart.data.datasets[1].data = deviceData[markerKey].malLineData; 
+    line_chart.update();
+
+    var newDoughnutData = genRandData(); 
+    deviceData[markerKey].doughnutData = newDoughnutData; 
+    
+    doughnut_chart.data.datasets[0].data = deviceData[markerKey].doughnutData;
+    doughnut_chart.update(); 
 }
 
 // close 버튼 클릭 이벤트
@@ -96,3 +147,24 @@ document.getElementById('closeBtn').onclick = function () {
     document.getElementById('sidebar').style.display = 'none';
     clearInterval(chartUpdateInterval); 
 };
+
+function generateRandomData() {
+    // 랜덤 데이터를 생성하는 함수 (예시)
+    return [
+        Math.floor(Math.random() * 25),
+        Math.floor(Math.random() * 25),
+        Math.floor(Math.random() * 25),
+        Math.floor(Math.random() * 25),
+        Math.floor(Math.random() * 25),
+        Math.floor(Math.random() * 25),
+        Math.floor(Math.random() * 25)
+    ];
+}
+
+function genRandData() {
+    return [
+        Math.floor(Math.random() * 25),
+        Math.floor(Math.random() * 25),
+        Math.floor(Math.random() * 25)
+    ]
+}
--- sidebar.js
+++ sidebar.js
@@ -4,13 +4,32 @@ function onMarkerClick(markerKey) {
     var device_name = document.getElementById("device_name");
     device_name.textContent=markerKey;
 
-    if (!line_chart) {
-        showLineChart();
-    }
-    if (!doughnut_chart) {
-        showdoughnutChart();
+    // 차트 초기화
+    if (chartUpdateInterval) {
+        clearInterval(chartUpdateInterval);
+        chartUpdateInterval = null;
     }
 
+    if (line_chart) {
+        line_chart.destroy();
+        line_chart = null;
+    }
+
+    if (doughnut_chart) {
+        doughnut_chart.destroy();
+        doughnut_chart = null;
+    }
+
+    if(pie_chart){
+        pie_chart.destroy();
+        pie_chart= null;
+    }
+
+    //차트 실행
+    showLineChart(markerKey);
+    showdoughnutChart(markerKey);
+    showPieChart(markerKey);
+    //차트 데이터 업데이트
     startChartUpdate(markerKey);
 }
 
